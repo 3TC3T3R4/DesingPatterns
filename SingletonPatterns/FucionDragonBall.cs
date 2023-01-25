15fe60532e0fe868ecb4d34afc35a3becb671bd1
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-25 13:38:46 -0500

    Implementacion
    Se realiza la implementacion del  patron de diseño singleton mas la la implementacion de la  explicacion en comentarios

--- SingletonPatterns/FucionDragonBall.cs
+++ SingletonPatterns/FucionDragonBall.cs
@@ -6,15 +6,46 @@ using System.Threading.Tasks;
 
 namespace SingletonPatterns
 {
-    public class Logger
+    public class FucionDragonBall
     {
-        private Logger()
+        //private static FucionDragonBall intancia = new FucionDragonBall();
+        private static FucionDragonBall instancia;
+        private static string valor;
+
+        //Se realiza constructor vacio para no instanciar mediante  como comun mente se hacia "new"
+        private FucionDragonBall()
         {
 
         }
-    
-    
-    
-    
+
+
+        public static FucionDragonBall getInstance() {
+
+            if (instancia == null) {
+
+               instancia= new FucionDragonBall();
+
+            }
+            return instancia;
+
+        }
+
+
+        public void hacerFucion() {
+
+
+            Console.WriteLine("FUSION!");
+        
+        }
+
+
+        public void deshacerFucion() {
+
+            Console.WriteLine("Se me acabo el kayoken");
+
+        }
+
+
+
     }
 }
--- SingletonPatterns/Program.cs
+++ SingletonPatterns/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿namespace SingletonPatterns {
+
+    class Program
+    {
+        private static void Main(string[] args){
+
+
+            FucionDragonBall fu = FucionDragonBall.getInstance();
+
+            FucionDragonBall fu2 = FucionDragonBall.getInstance();
+            
+            FucionDragonBall fu3 = FucionDragonBall.getInstance();
+
+            fu.hacerFucion();
+
+            fu2.deshacerFucion();
+
+            fu3.hacerFucion();
+
+            //Si hicieramos la creacion de la manera normal hariamos esto
+            /*
+             
+            FucionDragonBall fu =  new FucionDragonBall();
+
+            FucionDragonBall fu2 =  new FucionDragonBall();
+
+            FucionDragonBall fu3 =  new FucionDragonBall();
+
+             fu.hacerFucion();
+
+            fu2.deshacerFucion();
+
+            fu3.hacerFucion();
+
+             Tendriamos en memoria 3 instancias en vez de 1 con el patron sigleton
+
+             */
+
+        }
+
+    }
+    }
