77f4f391b2b49de684090b8a04e0da0edb46e503
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-25 15:23:21 -0500

    Creacion e Implementacion
    Se realizo la creacion del patron de diseño strategy y su respectiva implementacion

---
+++ StrategyPatterns/contexto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatterns
+{
+    public  class contexto{
+
+        private lNextStep proximoPaso;
+        
+        public contexto(lNextStep proximoPaso) { 
+        
+        this.proximoPaso = proximoPaso;
+        
+        
+        }
+
+        public void start() {
+
+            this.proximoPaso.finalStep();
+        
+        }
+
+
+    }
+}
---
+++ StrategyPatterns/stepSimple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatterns
+{
+    public abstract class stepSimple: lNextStep
+    {
+
+
+
+        public void finalStep()
+        {
+
+            smallstep();
+            mediunstep();
+            largestep();
+
+        }
+
+
+        public abstract void smallstep();
+        public abstract void mediunstep();
+        public abstract void largestep();
+
+
+
+
+
+    }
+}
